e03f8c8518f44931a194c348533154615c015086
Larissa Barra <lari_conde94@hotmail.com>
2017-04-15 19:09:57 -0300

    Servidor HTTP

---
+++ server/server.py
@@ -0,0 +1,38 @@
+from flask import Flask, render_template, request, jsonify
+import json
+import paho.mqtt.client as mqtt
+
+app = Flask(__name__)
+data = []
+
+@app.route('/')
+def capture():
+    return render_template ('index.html')
+
+@app.route('/getData')
+def getData():
+    return jsonify(data)
+
+# paho callbacks
+def on_connect(client, userdata, flags, rc):
+    # Start subscribe, with QoS level 2
+    client.subscribe("coordenadas", 2)
+    print("rc: " + str(rc))
+
+def on_subscribe(mosq, obj, mid, granted_qos):
+    print("Subscribed: " + str(mid) + " " + str(granted_qos))
+
+def on_message(mosq, obj, msg):
+    print(msg.topic + " " + str(msg.qos) + " " + str(msg.payload))
+    message = json.loads(msg.payload)
+    data.append({'sensor': message.sensor, 'location': message.location})
+
+if __name__=='__main__':
+    client = mqtt.Client()
+    client.on_connect = on_connect
+    client.on_subscribe = on_subscribe
+    client.on_message = on_message
+    client.connect("127.0.0.1", 1883, 60)
+    client.loop_start()
+
+    app.run(host="127.0.0.1", port=8080)
